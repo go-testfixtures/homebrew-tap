119fe9fb7390f206f1157ba18f90086bd6d40ada
Andrey Nering <andreynering@users.noreply.github.com>
2024-10-25 10:39:21 -0300

    release: v3.13.0

--- Formula/testfixtures.rb
+++ Formula/testfixtures.rb
@@ -1,14 +1,14 @@
 class Testfixtures < Formula
   desc "Ruby on Rails like test fixtures for Go. Write tests against a real database"
   homepage "https://github.com/go-testfixtures/testfixtures"
-  version "3.12.0"
+  version "3.13.0"
 
   if OS.mac?
     url "https://github.com/go-testfixtures/testfixtures/releases/download/v#{version}/testfixtures_darwin_amd64.tar.gz"
-    sha256 "420ac605b3b25187d5f2afaf6d100a6456b104c57cc77078f724dc6c0f4d927d"
+    sha256 "6f2a4131f81ded21411590ad0b51f6f4d0b692c49ecb61adc7cb0b943c1086bc"
   elsif OS.linux?
     url "https://github.com/go-testfixtures/testfixtures/releases/download/v#{version}/testfixtures_linux_amd64.tar.gz"
-    sha256 "53b826629738c5e1b18a1cdf346533317a7475c592d82cd4cf1781ae5947a85c"
+    sha256 "385044a7874074014cc67ebc79d86116cc277325f158fddd098c3e49c987cb1d"
   end
 
   def install
